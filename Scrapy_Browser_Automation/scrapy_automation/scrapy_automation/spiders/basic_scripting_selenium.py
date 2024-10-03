61114d0ccd9267623fd10a4e58c17dff04f8a908
Farhan <tusarcc@gmail.com>
2024-10-03 19:28:01 +0600

    commit

--- Scrapy_Browser_Automation/scrapy_automation/scrapy_automation/spiders/basic_scripting_selenium.py
+++ Scrapy_Browser_Automation/scrapy_automation/scrapy_automation/spiders/basic_scripting_selenium.py
@@ -1,3 +1,34 @@
+'''
+Settings.py configuration. We also downloaded the Selenium Chrome executable and placed it in the scrapy_automation folder.
+
+Scrapy_Browser_Automation\
+├── scrapy_automation\    
+│   └── chrome.exe  #Chrome executable
+├── scrapy_automation\          
+│   ├── __init__.py
+│   ├── items.py
+│   ├── middlewares.py
+│   ├── pipelines.py
+│   ├── settings.py
+│   └── spiders\
+│       ├── __init__.py
+│       ├── basic_scripting_playwright.py 
+│       └── basic_scripting_selenium.py
+└── scrapy.cfg          
+
+
+from shutil import which
+  
+SELENIUM_DRIVER_NAME = 'chrome'
+SELENIUM_DRIVER_EXECUTABLE_PATH = which('chromedriver')
+SELENIUM_DRIVER_ARGUMENTS=['--headless']  
+
+DOWNLOADER_MIDDLEWARES = {
+     'scrapy_selenium.SeleniumMiddleware': 800
+     }
+
+'''
+
 import scrapy
 from scrapy_selenium import SeleniumRequest
 
